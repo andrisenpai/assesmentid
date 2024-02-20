d44a821303b002ae35cbdb9d3681a658ab6a9469
Andri Nugroho <andri.nugroho@indocyber.id>
2024-02-20 18:56:17 +0700

    FEAT; UPDATE PRICELIST PRODUK

--- components/CardPrice.vue
+++ components/CardPrice.vue
@@ -1,33 +1,74 @@
 <template>
     <div class="section-padding-bottom" data-aos="fade-up" data-aos-delay="600">
-        <div class="container section-container-price">
+        <div class="container">
             <div class="row justify-content-center align-items-center">
-                <div class="card-container my-2 rounded" v-for="product in products">
-                    <div :class="['card', product.id == 1 ? 'populer' : '']">
-                    <!-- <div :class="['card']"> -->
-                        <div class="card-body">
-                            <h5 id="cardPriceTitle" class="card-price-title text-center mt-3 fw-bolder">{{ product.name }}</h5>
-                            <div class="row justify-content-center align-items-center mb-5">
-                                <div class="col">
-                                    <p class="text-center">{{ priceWord }}</p>
-                                    <h1 class="text-center">{{ product.price }}</h1>
+                <swiper :speed="1000" :loop="false" :navigation="true" :breakpoints="swiperOptions.breakpoints" :pagination="{
+                    clickable: true,
+                }">
+                    <swiper-slide class="card-container my-2 rounded" v-for="product in products">
+                        <div :class="['card', product.id == 1 ? 'populer' : '']">
+                            <div class="card-body">
+                                <h5 id="cardPriceTitle" class="card-price-title text-center mt-3 fw-bolder">{{ product.name
+                                }}</h5>
+                                <div class="row justify-content-center align-items-center mb-5">
+                                    <div class="col">
+                                        <p class="text-center">{{ priceWord }}</p>
+                                        <h1 class="text-center">{{ product.price }}</h1>
+                                    </div>
+                                </div>
+                                <div class="d-grid">
+                                    <nuxt-link :to="`/products/product-${product.id}/${product.id}`"
+                                        class="btn btn-block rounded" type="submit">{{ btnWord }}</nuxt-link>
                                 </div>
                             </div>
-                            <div class="d-grid">
-                                <nuxt-link :to="`/products/product-${product.id}/${product.id}`" class="btn btn-block rounded" type="submit">{{ btnWord }}</nuxt-link>
-                            </div>
                         </div>
-                    </div>
-                </div>
+                    </swiper-slide>
+                </swiper>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="js">
+import { Swiper, SwiperSlide } from "swiper/vue"
+import SwiperCore, { Navigation, Pagination } from 'swiper/core'
+SwiperCore.use([Navigation, Pagination])
 
-    export default {
+import "swiper/css"
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
     props: ["products", "priceWord", "btnWord"],
+    data() {
+        return {
+            activeItem: 'architecture',
+
+            swiperOptions: {
+                navigation: {
+                    nextEl: '.tab-carousel-next',
+                    prevEl: '.tab-carousel-prev',
+                },
+
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                    },
+                    992: {
+                        slidesPerView: 3,
+                    },
+                    1200: {
+                        slidesPerView: 4,
+                    }
+                }
+            },
+        }
+    },
     mounted() {
         const cards = document.querySelectorAll(".card-container .card");
         cards.forEach((element) => {
@@ -47,15 +88,20 @@
         })
 
     },
+
 }
 </script>
 
 <style lang="scss">
-    .card-price-title{
-        height: 100px;
-    }
+.card-price-title {
+    height: 100px;
+}
 
-    .display-block {
-        display: block;
-    }
+.display-block {
+    display: block;
+}
+
+.card-container {
+    margin: 0px;
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
     <NuxtErrorBoundary>
-    <div class="main-container">
-        <Loading v-if="loading"/>
-        <div v-else >
-            <TheHeader />
-            <OffCanvasMobileMenu />
-            <HeroOne :heros="bannerList" />
-            <AboutOne :about="aboutData"/>
-            <Features :featureProps="features" :title="featureTitle"/>
-            <GalleryOne :title="productTitle" :subtitle="productSubtitle" :products="productList" />
-            <Order :orders="orders" :title="orderTitle" :subtitle="orderSubtitle"/>
-            <BlogStyleOne :blogs="articleList" :title="articleTitle" :subtitle="articleSubtitle" />
-            <FooterNew/>
-            <FooterSimple/>
+        <div class="main-container">
+            <Loading v-if="loading" />
+            <div v-else>
+                <TheHeader />
+                <OffCanvasMobileMenu />
+                <HeroOne :heros="bannerList" />
+                <AboutOne :about="aboutData" />
+                <Features :featureProps="features" :title="featureTitle" />
+                <GalleryOne :title="productTitle" :subtitle="productSubtitle" :products="productList" />
+                <Order :orders="orders" :title="orderTitle" :subtitle="orderSubtitle" />
+                <BlogStyleOne :blogs="articleList" :title="articleTitle" :subtitle="articleSubtitle" />
+                <FooterNew />
+                <FooterSimple />
+            </div>
         </div>
-    </div>
     </NuxtErrorBoundary>
 </template>
 
@@ -23,175 +23,174 @@ import Aos from 'aos'
 import { transformDate } from '~~/utils/helper'
 
 export default {
-        async setup(){
-            const loading = ref(false)
-            const footerStore = useFooterStore()
-            const aboutStore = useAboutStore()
-            
-            const {
-                language, 
-            } = useNavigationStore()                
-            
-            const articleStore = useArticleStore()
-            const productStore = useProductStore()
-            
-            const {
-                addProduct,
-                productList,
-             } = productStore
-            console.log(productList)
-             const {    
-                addArticle, articleList, 
-            } = articleStore
-            
-            const footers = footerStore.$state
-            const aboutData = aboutStore.$state
-            const config = useRuntimeConfig()
-            
-            var bannerList : BannerType[] = []
-                
-            loading.value = true
-            
-            const [
-                {data : banners},
-                {data, pending: pendingData}, 
-                {data : articlesItem, pending : pendingArticle},
-            ] = await Promise.all([
-                useFetch('/api/homeBanners'),
-                useFetch('/api/home', {
-                    params: {locale : `${language}`}
-                }),
-                useFetch('/api/articles?sort=publishedAt%3Aasc&pagination[page]=1&pagination[pageSize]=4&populate=deep,2'),
-            ])
-            
-            footerStore.$patch({
-                address : data.value.attributes.footer.address,
-                copyright : data.value.attributes.footer.copyright,
-                email_1 : data.value.attributes.footer.email_1,
-                email_2 : data.value.attributes.footer.email_2,
-                facebook : data.value.attributes.footer.facebook,
-                instagram : data.value.attributes.footer.instagram,
-                no_telepon : data.value.attributes.footer.no_telepon,
-                twitter : data.value.attributes.footer.twitter,
-                wa_1 : data.value.attributes.footer.wa_1,
-                wa_2 : data.value.attributes.footer.wa_2,
-            })
+    async setup() {
+        const loading = ref(false)
+        const footerStore = useFooterStore()
+        const aboutStore = useAboutStore()
 
-            banners.value.forEach((data : any) => {
-                bannerList.push({
-                    id : data.id,
-                    background : config.public.clientCmsBaseUrl + data.attributes?.background?.data?.attributes?.url,
-                    title_1 : data.attributes?.title_1,
-                    title_2 : data.attributes?.title_2,
-                    subtitle : data.attributes?.subtitle,
-                    currency : data.attributes?.currency,
-                    price : data.attributes?.price,
-                    text_info : data.attributes?.text_info,
-                    phone_numbers : data.attributes?.phone_numbers,
-                    points : data.attributes?.points,
-                })
-            });
+        const {
+            language,
+        } = useNavigationStore()
 
-            aboutStore.$patch({
-                title : data.value.attributes.about_us.title,
-                subtitle : data.value.attributes.about_us.subtitle,
-                paragraph : data.value.attributes.about_us.description,
-                sideImage : config.public.clientCmsBaseUrl + data.value.attributes.about_us.image.data[0].attributes.url,
-                sideImage2 : config.public.clientCmsBaseUrl + data.value.attributes.about_us.image.data[1].attributes.url
-            })
+        const articleStore = useArticleStore()
+        const productStore = useProductStore()
 
-            const features = [] as any[]
-            const featureFetched = data.value.attributes.feature.features
-            featureFetched.forEach((value : any) => {
-                features.push(
-                    {
-                        id : value.id,
-                        title : value.title,
-                        description : value.description,
-                        icon : config.public.clientCmsBaseUrl +  value.icon.data.attributes.url
-                    }
-                )
-            })
-            
-            const orders = [] as any[]
-            const ordersFetched = data.value.attributes.order
-            ordersFetched.forEach((value : any) => {
-                orders.push(
-                    {
-                        title : value.title,
-                        text : value.button,
-                        link : value.link_button,
-                        src : config.public.clientCmsBaseUrl + value.img.data.attributes.url
-                    }
-                )
-            })
-            
-            
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            articlesItem.value.articleList.forEach((value : any) => {
-                addArticle({
-                    id : value.id,
-                    title : value.attributes.title,
-                    category : value.attributes?.article_category.data.attributes.title,
-                    imageUrl : config.public.clientCmsBaseUrl + value.attributes.image_thumbnail_small.data.attributes.url,
-                    publishedDate : transformDate(value.attributes.publishedAt),
-                    slug : value.attributes.slug
-                })
-            })
+        const {
+            addProduct, productList,
+        } = productStore
 
-            const productFethed = data.value.attributes.product
-            productFethed.products.forEach((value : any) => {
-                addProduct({
-                    title : value.title,
-                    subtitle : value.subtitle,
-                    imageUrl : config.public.clientCmsBaseUrl +  value.imgSrc.data.attributes.url
-                })
-            })
-            const articleTitle = data.value.attributes.article_section_title
-            const articleSubtitle = data.value.attributes.article_section_subtitle
-            const featureTitle = data.value.attributes.feature.section_title
-            const orderTitle = data.value.attributes.order_section_title
-            const orderSubtitle = data.value.attributes.order_section_subtitle
-            const productTitle = productFethed.section_title
-            const productSubtitle = productFethed.section_subtitle
+        const {
+            addArticle, articleList,
+        } = articleStore
 
-            return {
-                footers,
-                aboutData,
-                features,
-                articleList,
-                articleTitle,
-                articleSubtitle,
-                featureTitle,
-                productTitle,
-                productSubtitle,
-                productList,
-                language,
-                orders,
-                orderTitle,
-                orderSubtitle,
-                pendingData,
-                pendingArticle,
-                loading,
-                bannerList
-            }
+        const footers = footerStore.$state
+        const aboutData = aboutStore.$state
+        const config = useRuntimeConfig()
+
+        var bannerList: BannerType[] = []
+
+        loading.value = true
+
+        const [
+            { data: banners },
+            { data, pending: pendingData },
+            { data: articlesItem, pending: pendingArticle },
+        ] = await Promise.all([
+            useFetch('/api/homeBanners'),
+            useFetch('/api/home', {
+                params: { locale: `${language}` }
+            }),
+            useFetch('/api/articles?sort=publishedAt%3Aasc&pagination[page]=1&pagination[pageSize]=4&populate=deep,2'),
+        ])
+
+        footerStore.$patch({
+            address: data.value.attributes.footer.address,
+            copyright: data.value.attributes.footer.copyright,
+            email_1: data.value.attributes.footer.email_1,
+            email_2: data.value.attributes.footer.email_2,
+            facebook: data.value.attributes.footer.facebook,
+            instagram: data.value.attributes.footer.instagram,
+            no_telepon: data.value.attributes.footer.no_telepon,
+            twitter: data.value.attributes.footer.twitter,
+            wa_1: data.value.attributes.footer.wa_1,
+            wa_2: data.value.attributes.footer.wa_2,
+        })
+
+        banners.value.forEach((data: any) => {
+            bannerList.push({
+                id: data.id,
+                background: config.public.clientCmsBaseUrl + data.attributes?.background?.data?.attributes?.url,
+                title_1: data.attributes?.title_1,
+                title_2: data.attributes?.title_2,
+                subtitle: data.attributes?.subtitle,
+                currency: data.attributes?.currency,
+                price: data.attributes?.price,
+                text_info: data.attributes?.text_info,
+                phone_numbers: data.attributes?.phone_numbers,
+                points: data.attributes?.points,
+            })
+        });
+
+        aboutStore.$patch({
+            title: data.value.attributes.about_us.title,
+            subtitle: data.value.attributes.about_us.subtitle,
+            paragraph: data.value.attributes.about_us.description,
+            sideImage: config.public.clientCmsBaseUrl + data.value.attributes.about_us.image.data[0].attributes.url,
+            sideImage2: config.public.clientCmsBaseUrl + data.value.attributes.about_us.image.data[1].attributes.url
+        })
+
+        const features = [] as any[]
+        const featureFetched = data.value.attributes.feature.features
+        featureFetched.forEach((value: any) => {
+            features.push(
+                {
+                    id: value.id,
+                    title: value.title,
+                    description: value.description,
+                    icon: config.public.clientCmsBaseUrl + value.icon.data.attributes.url
+                }
+            )
+        })
+
+        const orders = [] as any[]
+        const ordersFetched = data.value.attributes.order
+        ordersFetched.forEach((value: any) => {
+            orders.push(
+                {
+                    title: value.title,
+                    text: value.button,
+                    link: value.link_button,
+                    src: config.public.clientCmsBaseUrl + value.img.data.attributes.url
+                }
+            )
+        })
+
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        articlesItem.value.articleList.forEach((value: any) => {
+            addArticle({
+                id: value.id,
+                title: value.attributes.title,
+                category: value.attributes?.article_category.data.attributes.title,
+                imageUrl: config.public.clientCmsBaseUrl + value.attributes.image_thumbnail_small.data.attributes.url,
+                publishedDate: transformDate(value.attributes.publishedAt),
+                slug: value.attributes.slug
+            })
+        })
+
+        const productFethed = data.value.attributes.product
+        productFethed.products.forEach((value: any) => {
+            addProduct({
+                title: value.title,
+                subtitle: value.subtitle,
+                imageUrl: config.public.clientCmsBaseUrl + value.imgSrc.data.attributes.url
+            })
+        })
+        const articleTitle = data.value.attributes.article_section_title
+        const articleSubtitle = data.value.attributes.article_section_subtitle
+        const featureTitle = data.value.attributes.feature.section_title
+        const orderTitle = data.value.attributes.order_section_title
+        const orderSubtitle = data.value.attributes.order_section_subtitle
+        const productTitle = productFethed.section_title
+        const productSubtitle = productFethed.section_subtitle
+
+        return {
+            footers,
+            aboutData,
+            features,
+            articleList,
+            articleTitle,
+            articleSubtitle,
+            featureTitle,
+            productTitle,
+            productSubtitle,
+            productList,
+            language,
+            orders,
+            orderTitle,
+            orderSubtitle,
+            pendingData,
+            pendingArticle,
+            loading,
+            bannerList
+        }
+    },
+    data() {
+        return {
+            img1: false,
+            img2: false,
+            img3: false
+        }
+    },
+    methods: {
+        preloadImage(url: string, option: number) {
+            const image = new Image()
+            image.src = url
+            image.onload = () => { this.handleLoadImage(option) }
         },
-        data(){
-            return {
-                img1 : false,
-                img2 : false,
-                img3 : false
-            }
-        },
-        methods : {
-            preloadImage(url : string, option : number) {
-                const image = new Image()
-                image.src = url
-                image.onload = () => {this.handleLoadImage(option)}
-            },
-            handleLoadImage(option : number){
-             switch (option) {
+        handleLoadImage(option: number) {
+            switch (option) {
                 case 1:
                     this.img1 = true
                     break;
@@ -203,39 +202,39 @@ export default {
                     break;
                 default:
                     break;
-             }
+            }
 
-             if(this.img1 && this.img2 && this.img3){
+            if (this.img1 && this.img2 && this.img3) {
                 this.handleLoading()
-             }
-            },
-            handleLoading(){
-                this.loading = false
             }
         },
-        async mounted(){
-            try {
-                Aos.init()                            
-                window.scroll({
-                    top: 0,
-                    behavior: "smooth",
-                })  
-            } catch (error) {
-                console.log("failed : ", error) 
-            }
-
-            try {
-                this.bannerList.map((hero, index) => {
-                    const num = index + 1
-                    this.preloadImage(hero.background, num)
-                    console.log(`url ${num} : ${hero.background}`)
-                })
-            } catch (error) {
-                console.log("error preload : ", error)
-            }
+        handleLoading() {
             this.loading = false
         }
-    } 
+    },
+    async mounted() {
+        try {
+            Aos.init()
+            window.scroll({
+                top: 0,
+                behavior: "smooth",
+            })
+        } catch (error) {
+            console.log("failed : ", error)
+        }
+
+        try {
+            this.bannerList.map((hero, index) => {
+                const num = index + 1
+                this.preloadImage(hero.background, num)
+                console.log(`url ${num} : ${hero.background}`)
+            })
+        } catch (error) {
+            console.log("error preload : ", error)
+        }
+        this.loading = false
+    }
+} 
 </script>
 
 
